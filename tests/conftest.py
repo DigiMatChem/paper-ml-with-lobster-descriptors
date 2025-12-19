33a82b3b6c8fb968de24f060d03b89090fbd14d4
naik-aakash <aakash.naik@bam.de>
2025-12-19 10:38:44 +0100

    add conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,12 +1,14 @@
 """
 Fixtures for tests
 """
+
 from pathlib import Path
 import pytest
+
 
 @pytest.fixture(scope="session")
 def data_dir():
     """
     Absolute path of data directory.
     """
-    return (Path(__file__).resolve().parents[1] / "data")
+    return Path(__file__).resolve().parents[1] / "data"
