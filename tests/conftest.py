1f6eae2a79be0c9483dfdf9a3b01acef750497f3
Aakash Ashok Naik <91958822+naik-aakash@users.noreply.github.com>
2025-12-31 12:04:53 +0000

    add fixtures

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,7 @@
 """
 Fixtures for tests
 """
-
+import psutil
 from pathlib import Path
 import pytest
 
@@ -12,3 +12,20 @@ def data_dir():
     Absolute path of data directory.
     """
     return Path(__file__).resolve().parents[1] / "data"
+
+
+@pytest.fixture(scope="session")
+def test_data_dir():
+    """
+    Absolute path of test_data directory.
+    """
+    module_dir = Path(__file__).parent.resolve()
+    test_dir = module_dir.joinpath("test_data")
+    return test_dir.resolve()
+
+@pytest.fixture(scope="session")
+def num_jobs():
+    """
+    Number of processors available for testing.
+    """
+    return psutil.cpu_count(logical=False) or 1
