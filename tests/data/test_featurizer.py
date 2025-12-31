b08155e0e687cf05d662998467267d1762f51006
Aakash Ashok Naik <91958822+naik-aakash@users.noreply.github.com>
2025-12-31 12:06:23 +0000

    add data module tests

---
+++ tests/data/test_featurizer.py
@@ -0,0 +1,81 @@
+import pandas as pd
+from mlproject.data.featurizer import (
+    get_matminer_feats,
+    get_matminer_site_feats,
+    get_lobster_feats,
+    get_lobster_site_feat,
+)
+
+
+def test_get_matminer_feats(data_dir, num_jobs):
+    print(num_jobs)
+    data_parent_dir = data_dir
+
+    structures_file = data_parent_dir / "structures" / "structures.json.gz"
+
+    structures_df = pd.read_json(structures_file).head(5)
+
+    matminer_feats = get_matminer_feats(structures_df=structures_df, n_jobs=num_jobs)
+
+    assert isinstance(matminer_feats, pd.DataFrame)
+    assert matminer_feats.shape[0] == structures_df.shape[0]
+
+
+def test_get_matminer_site_feats(data_dir, num_jobs):
+    data_parent_dir = data_dir
+
+    structures_file = data_parent_dir / "structures" / "structures.json.gz"
+
+    structures_df = pd.read_json(structures_file).head(5)
+    structures_df["site_index"] = 0
+
+    matminer_site_feats = get_matminer_site_feats(
+        structures_df=structures_df, n_jobs=num_jobs
+    )
+
+    assert isinstance(matminer_site_feats, pd.DataFrame)
+    assert matminer_site_feats.shape[0] == structures_df.shape[0]
+
+
+def test_get_lobster_feats(test_data_dir, num_jobs):
+
+    data_parent_dir = test_data_dir
+
+    path_to_lobster_calcs = data_parent_dir / "lobster_calcs"
+
+    lobster_feats = get_lobster_feats(
+        path_to_lobster_calcs=path_to_lobster_calcs, n_jobs=num_jobs
+    )
+
+    assert isinstance(lobster_feats, pd.DataFrame)
+    assert sorted(lobster_feats.index) == sorted(["mp-463", "mp-1000", "mp-2176"])
+    assert lobster_feats.shape[0] == 3  # number of lobster calc folders
+    assert lobster_feats.shape[1] == 135  # number of features extracted
+
+
+def test_get_lobster_site_feat(test_data_dir):
+    data_parent_dir = test_data_dir
+
+    path_to_lobster_calc = data_parent_dir / "lobster_calcs" / "mp-2176"
+
+    lobster_site_feats = get_lobster_site_feat(
+        path_to_lobster_calc=path_to_lobster_calc,
+        site_index=1,
+    )
+
+    expected_cols = [
+        "site_bwdf_mean",
+        "site_bwdf_std",
+        "site_bwdf_min",
+        "site_bwdf_max",
+        "site_bwdf_sum",
+        "site_bwdf_skew",
+        "site_bwdf_kurtosis",
+        "site_asi",
+        "charge_loew",
+        "charge_mull",
+    ]
+
+    assert isinstance(lobster_site_feats, pd.DataFrame)
+    assert lobster_site_feats.index == "mp-2176_1"
+    assert all(col in lobster_site_feats.columns for col in expected_cols)
---
+++ tests/data/test_preprocessing.py
@@ -0,0 +1,49 @@
+from mlproject.data.preprocessing import get_dataset
+
+
+def test_get_dataset(data_dir):
+    data_parent_dir = data_dir
+
+    target_name = "max_pfc"
+
+    target_df, feature_df = get_dataset(
+        target_name=target_name,
+        feat_type="matminer",
+        data_parent_dir=data_parent_dir,
+    )
+
+    assert target_df.shape[0] == feature_df.shape[0]
+    assert target_df.columns[0] == target_name
+    assert target_df.index.equals(feature_df.index)
+
+    # check that only matminer features are present
+    assert not any(col.startswith("asi_") for col in feature_df.columns)
+    assert not any(col.startswith("bwdf_") for col in feature_df.columns)
+    assert not any(col.startswith("ICOHP_") for col in feature_df.columns)
+
+    # check if units are correctly applied
+    assert any("Average_bond_length (A)" in c for c in feature_df.columns)
+    assert any("Average_bond_angle (rad)" in c for c in feature_df.columns)
+    assert any("GSbandgap (eV)" in c for c in feature_df.columns)
+    assert any("LUMO_energy (eV)" in c for c in feature_df.columns)
+    assert any("CovalentRadius (pm)" in c for c in feature_df.columns)
+
+    # check for lobster features when feat_type is matminer_lob
+    _, feature_df_lob = get_dataset(
+        target_name=target_name,
+        feat_type="matminer_lob",
+        data_parent_dir=data_parent_dir,
+    )
+    assert any(col.startswith("asi_") for col in feature_df_lob.columns)
+    assert any(col.startswith("bwdf_") for col in feature_df_lob.columns)
+    assert any(col.startswith("Icohp_") for col in feature_df_lob.columns)
+
+    # check if units are correctly applied for lobster features
+    assert any("w_ICOHP (eV)" in c for c in feature_df_lob.columns)
+    assert any("asi_std (eV)" in c for c in feature_df_lob.columns)
+    assert any("Icohp_mean_min (eV)" in c for c in feature_df_lob.columns)
+    assert any("bwdf_sum (eV)" in c for c in feature_df_lob.columns)
+    assert any("w_ICOHP (eV)" in c for c in feature_df_lob.columns)
+    assert any("Madelung_Mull (eV)" in c for c in feature_df_lob.columns)
+    assert any("center_COHP (eV)" in c for c in feature_df_lob.columns)
+    assert any("dist_at_neg_bwdf2 (A)" in c for c in feature_df_lob.columns)
